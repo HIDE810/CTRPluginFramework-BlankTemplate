f01e7f97791100e0e1f0ee64ab7e5e7bd4b7a4d4
HIDE <48782538+HIDE810@users.noreply.github.com>
2020-06-08 08:49:21 +0900

    Update flash.cpp

--- Sources/flash.cpp
+++ Sources/flash.cpp
@@ -1,24 +1,18 @@
 #include "common.hpp"
 
-#define REG32(addr) (*(vu32 *)((void *)((u32)(addr) | 1 << 31)))
-
 namespace CTRPluginFramework
 {
-	void Flash(u32 color)
-	{
-		color |= 0x01000000;
-		for (u32 i = 0; i < 64; i++)
-		{
-			REG32(0x10202204) = color;
-			svcSleepThread(10000000);
-		}
-	}
-	
 	void RandomFlash(MenuEntry *entry)
 	{
+		int *p = (int *)0x90202204;
+		
 		if (entry->Hotkeys[0].IsDown())
-			Flash(Utils::Random(0, 0xFFFFFF));
-		else
-			REG32(0x10202204) = 0;
+		{
+			*p = Utils::Random(0, 0xFFFFFF) | 0x01000000;
+			
+			for (u32 i = 0; i < 64; i++)
+				svcSleepThread(10000000);
+		}
+		else *p = 0;
 	}
 }
