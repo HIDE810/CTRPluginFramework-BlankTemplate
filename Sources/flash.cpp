29d62c96299fde95f87740f4d6f8be9132380aa8
HIDE <48782538+HIDE810@users.noreply.github.com>
2020-06-10 12:24:55 +0900

    Update flash.cpp

--- Sources/flash.cpp
+++ Sources/flash.cpp
@@ -2,18 +2,36 @@
 
 namespace CTRPluginFramework
 {
-    int *p = (int *)0x90202204;
+    //https://www.3dbrew.org/wiki/Memory_layout
+    u32 physaddr = 0x80000000;
+ 
+    //https://www.3dbrew.org/wiki/LCD_Registers
+    u32 *top = (u32 *)(physaddr + 0x10202204);
+    u32 *bottom = (u32 *)(physaddr + 0x10202A04);
+    
+    void Flash(u32 color)
+    {
+        //Flash flag
+        u32 enableFlash = 0x1000000;
+        
+        *top = enableFlash + color;
+        *bottom = enableFlash + color;
+    }
     
     void RandomFlash(MenuEntry *entry)
     {
         if (entry->Hotkeys[0].IsDown())
         {
-            *p = Utils::Random(0, 0xFFFFFF) + 0x1000000;
+            Flash(Utils::Random(0, 0xFFFFFF));
 
             //Sleep thread for 1.0s
             svcSleepThread(1000000000);
         }
-        else *p = 0;
+        else
+        {
+            *top = 0;
+            *bottom = 0;
+        }
     }
 
     void TouchFlash(MenuEntry *entry)
@@ -21,11 +39,19 @@ namespace CTRPluginFramework
         touchPosition tp;
         hidTouchRead(&tp);
         
-        u32 color = (0xFFFFFF/76800) * tp.px * tp.py + 0x1000000;
+        u32 pos = (0xFFFFFF/76800) * tp.px * tp.py;
 
-        *p = Controller::IsKeyDown(Touchpad)? color : 0;
-
-        //Sleep thread for 0.1s
-        svcSleepThread(100000000);
+        if(Controller::IsKeyDown(Touchpad))
+        {
+            Flash(pos);
+            
+            //Sleep thread for 0.1s
+            svcSleepThread(100000000);
+        }
+        else
+        {
+            *top = 0;
+            *bottom = 0;
+        }
     }
 }
